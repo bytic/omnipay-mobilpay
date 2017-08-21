55ccaa8aaab9ee1a601e59ba01971e2e9b934cfa
Gabriel Solomon <hello@gabrielsolomon.ro>
2017-08-21 16:45:10 +0300

    initial add complete purchase messages

---
+++ src/Message/CompletePurchaseRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ByTIC\Omnipay\Mobilpay\Message;
+
+/**
+ * Class PurchaseResponse
+ * @package ByTIC\Common\Payments\Gateways\Providers\AbstractGateway\Messages
+ */
+class CompletePurchaseRequest extends AbstractRequest
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function getData()
+    {
+        $data = [];
+
+        return $data;
+    }
+}
